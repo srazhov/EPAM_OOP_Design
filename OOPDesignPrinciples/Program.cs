ccc36e4e02fd09fcfc1d973eedbaffda550c4102
srazhov <65289085+srazhov@users.noreply.github.com>
2020-10-11 20:52:06 +0600

    Initial Commit

---
+++ OOPDesignPrinciples/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OOPDesignPrinciples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
